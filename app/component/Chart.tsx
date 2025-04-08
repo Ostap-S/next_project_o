ded78edcc11a33eba113a3a8d948f88d235d5bd2
OstapSavyskyi <ostap.v.savytskyi@ukd.edu.ua>
2025-04-08 03:25:40 +0300

    feat: Add Chart

---
+++ app/component/Chart.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { LineChart, Line } from 'recharts';
+
+const data = [
+  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+];
+
+const Chart = () => {
+  console.log("Rendering Chart!");
+
+  return (
+    <div>
+      <h2>Chart should appear below:</h2>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      </LineChart>
+    </div>
+  );
+};
+
+export default Chart;
--- app/component/hello.tsx
+++ app/component/hello.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import Chart from './Chart';
+
 export const Hello = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      Hello B.Dimass!
+      Hello Dimass!
     </div>
   );
 };
